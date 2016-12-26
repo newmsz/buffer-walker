77fc76290f9173925f774758b8cc7f932ee78847
Michal Molawka <michal.molawka@sonovum.de>
2016-12-26 11:48:08 +0100

    added tests for floats and constructor

--- test/tests.js
+++ test/tests.js
@@ -22,7 +22,37 @@ describe('BufferWalker', function(){
 		0, 0, 0, 0, 0, 0, 0, 1, // i64
 	]);
 
-	it('should read LE integers',function(){
+	it('should accept only instances of Buffer class', () =>  {
+		expect(() => {
+			new BufferWalker();
+		}).to.throw(TypeError);
+
+		expect(() => {
+			new BufferWalker(null);
+		}).to.throw(TypeError);
+
+		expect(() => {
+			new BufferWalker({});
+		}).to.throw(TypeError);
+
+		expect(() => {
+			new BufferWalker({ x: 1 });
+		}).to.throw(TypeError);
+
+		expect(() => {
+			new BufferWalker([]);
+		}).to.throw(TypeError);
+
+		expect(() => {
+			new BufferWalker([ 0x01, 0x02, 0x03, 0x04 ]);
+		}).to.throw(TypeError);
+
+		expect(() => {
+			new BufferWalker(Buffer.alloc(4));
+		}).not.to.throw(TypeError);
+	});
+
+	it('should read LE integers', () =>  {
 		const walker = new BufferWalker(bufferLE);
 
 		expect(walker.readInt32LE()).to.equal(1);
@@ -34,7 +64,7 @@ describe('BufferWalker', function(){
 		expect(walker.readInt64LE()).to.equal(1);
 	});
 
-	it('should read BE integers',function(){
+	it('should read BE integers', () =>  {
 		const walker = new BufferWalker(bufferBE);
 
 		expect(walker.readInt32BE()).to.equal(1);
@@ -76,5 +106,93 @@ describe('BufferWalker', function(){
 		walker.writeInt64BE(1);
 
 		expect(bufferBE).to.deep.equal(buffer);
+	});
+
+	it('should read LE floats', () =>  {
+		const buffer = Buffer.from([
+			0x8B, 0x94, 0x23, 0x44,
+		]); // 654.321
+
+		const walker = new BufferWalker(buffer);
+
+		expect(walker.readFloatLE()).to.be.closeTo(654.321, 0.001);
+	});
+
+	it('should read BE floats', () =>  {
+		const buffer = Buffer.from([
+			0x44, 0x23, 0x94, 0x8B,
+		]); // 654.321
+
+		const walker = new BufferWalker(buffer);
+
+		expect(walker.readFloatBE()).to.be.closeTo(654.321, 0.001);
+	});
+
+	it('should read LE floats', () =>  {
+		const buffer = Buffer.alloc(4);
+		const result = Buffer.from([ 0x8B, 0x94, 0x23, 0x44 ]); // 654.321
+
+		const walker = new BufferWalker(buffer);
+		walker.writeFloatLE(654.321);
+
+		expect(buffer).to.deep.equal(result);
+	});
+
+	it('should read BE floats', () =>  {
+		const buffer = Buffer.alloc(4);
+		const result = Buffer.from([ 0x44, 0x23, 0x94, 0x8B ]); // 654.321
+
+		const walker = new BufferWalker(buffer);
+		walker.writeFloatBE(654.321);
+
+		expect(buffer).to.deep.equal(result);
+	});
+
+	it('should read LE doubles',function(){
+		const buffer = Buffer.from([
+			0x77, 0xBE, 0x9F, 0x1A,
+			0x2F, 0xDD, 0x5E, 0x40,
+		]); // 123.456
+
+		const walker = new BufferWalker(buffer);
+
+		expect(walker.readDoubleLE()).to.be.closeTo(123.456, 0.001);
+	});
+
+	it('should read BE doubles', () =>  {
+		const buffer = Buffer.from([
+			0x40, 0x5E, 0xDD, 0x2F,
+			0x1A, 0x9F, 0xBE, 0x77,
+		]); // 123.456
+
+		const walker = new BufferWalker(buffer);
+
+		expect(walker.readDoubleBE()).to.be.closeTo(123.456, 0.001);
+	});
+
+	it('should write LE doubles', () =>  {
+		const buffer = Buffer.alloc(8);
+		const result = Buffer.from([
+			0x77, 0xBE, 0x9F, 0x1A,
+			0x2F, 0xDD, 0x5E, 0x40,
+		]); // 123.456
+
+		const walker = new BufferWalker(buffer);
+		walker.writeDoubleLE(123.456);
+
+		expect(buffer).to.deep.equal(result);
+	});
+
+	it('should write BE doubles', () =>  {
+		const buffer = Buffer.alloc(8);
+		const result = Buffer.from([
+			0x40, 0x5E, 0xDD, 0x2F,
+			0x1A, 0x9F, 0xBE, 0x77,
+		]); // 123.456
+
+		const walker = new BufferWalker(buffer);
+		walker.writeDoubleBE(123.456);
+
+		expect(buffer).to.deep.equal(result);
 	});
 });
